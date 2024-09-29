5d4a13d6e70d0a36d1e40dec5571f13b1d408a58
Olivér Mrakovics <oliver.mrakovics@gmail.com>
2024-09-29 14:57:12 +0200

    feat: add caret

--- src/components/terminal/TerminalPromptInput.tsx
+++ src/components/terminal/TerminalPromptInput.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, FormEvent, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface TerminalPromptInputProps {}
@@ -6,9 +6,17 @@ interface TerminalPromptInputProps {}
 const TerminalPromptInput: FC<TerminalPromptInputProps> = ({}) => {
   const [input, setInput] = useState("");
   const [inputFocus, setInputFocus] = useState(false);
+  const [caretPosition, setCaretPosition] = useState(0);
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    if (input.trim() === "") return;
+
+    setInput("");
+  }
 
   return (
-    <div className="relative w-full">
+    <form onSubmit={handleSubmit} className="relative w-full">
       <input
         autoFocus
         onFocus={() => setInputFocus(true)}
@@ -17,19 +25,28 @@ const TerminalPromptInput: FC<TerminalPromptInputProps> = ({}) => {
           e.target.focus();
         }}
         value={input}
-        onChange={(e) => setInput(e.target.value)}
+        onClick={(e) => setCaretPosition(e.currentTarget.selectionStart ?? 0)}
+        onKeyDown={(e) => setCaretPosition(e.currentTarget.selectionStart ?? 0)}
+        onChange={(e) => {
+          setInput(e.target.value);
+          setCaretPosition(e.target.selectionStart ?? 0);
+        }}
         className="w-full whitespace-nowrap bg-transparent p-0 text-kali-white text-transparent focus:outline-none"
       />
       <div
         className={twMerge(
-          "pointer-events-none absolute left-0 top-1/2 h-[20px] max-w-full -translate-y-1/2 overflow-hidden overflow-ellipsis whitespace-nowrap pr-2",
-          inputFocus &&
-            "after:content[''] after:absolute after:h-[20px] after:w-2 after:animate-blink after:bg-kali-gray/[.8]"
+          "pointer-events-none absolute left-0 top-1/2 h-[20px] max-w-full -translate-y-1/2 overflow-hidden overflow-ellipsis whitespace-nowrap pr-2"
         )}
       >
+        {inputFocus && (
+          <span
+            className="content[''] absolute top-0 h-[20px] w-2 animate-blink bg-kali-gray/[.8]"
+            style={{ left: `${input.length === 0 ? 0 : caretPosition}ch` }}
+          />
+        )}
         {input}
       </div>
-    </div>
+    </form>
   );
 };
 
